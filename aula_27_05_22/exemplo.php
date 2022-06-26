4d94cf4c8711e54cbc9ce8abc654e49c428d06bc
Raíssa Bespalec Daloia <79861204+rahbespalec@users.noreply.github.com>
2022-06-26 15:05:17 -0300

    Update exemplo.php

--- aula_27_05_22/exemplo.php
+++ aula_27_05_22/exemplo.php
@@ -39,5 +39,49 @@
     echo"<br>";
     echo "<hr>";
    
+// Utilizando a função 4 novamente 
+echo "REUTILIZANDO A FUNÇÃO <br> <br>";
 
+$outro = soma(55, 5);
+echo $outro;
+
+echo "<br>";
+echo "<hr>";
+
+// Exemplo 05
+echo "EXEMPLO 05 - CRIANDO FUNÇÃO COM VETORES <br> <br>";
+
+$v = [1, 3, 4, 5, 63, 4, 0, 2];
+
+function somar_v($entrada){
+    $s = 0;
+    for($i = 0; $i < count($entrada); $i++){
+        $s += $entrada[$i];
+    }
+    echo $s;
+}
+
+somar_v($v);
+
+echo "<br>";
+echo "<hr>";
+
+// Exemplo 06
+echo "EXEMPLO 06 - FUNÇÃO QUE VERIFICA ÍMPARES E PARES <BR> <BR>";
+
+$v = [1, 3, 4, 65, 63, 4, 10, 2];
+
+function p_i($entrada){
+    for($i = 0; $i < count($entrada); $i++){
+        if($entrada[$i] % 2 == 0){
+            echo "$entrada[$i] = Par <br>";
+        }
+        else{
+            echo "$entrada[$i] = Ímpar <br>";
+        }
+    }
+}
+
+p_i($v);
+?>
 ?>
